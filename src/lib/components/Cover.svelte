943258feb1c9923a5d4984286df50e34940ec271
dhruvbhatia00 <61220727+dhruvbhatia00@users.noreply.github.com>
2026-02-03 02:39:47 -0800

    small change

--- src/lib/components/Cover.svelte
+++ src/lib/components/Cover.svelte
@@ -66,7 +66,7 @@
     max-width: 600px;
     aspect-ratio: 5 / 7;
     box-shadow: 0 10px 40px rgba(0, 0, 0, 0.2);
-    background: white;
+    background: rgba(255, 255, 255, 0);
     position: relative;
     border-radius: 4px;
     
@@ -80,6 +80,7 @@
     display: flex;
     flex-direction: column;
     overflow: hidden;
+    border-radius: 6px;
   }
 
   /* Shared vertical line logic */
